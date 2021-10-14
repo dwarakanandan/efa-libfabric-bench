80340a7658927712d2a9330a2a7cb6dba3164ada
Dwarakanandan <dwarakanandan.bm@gmail.com>
2021-10-14 22:56:25 +0200

    Lib includes

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
+
+#include <ofi_osd.h>
+
+#include <rdma/fabric.h>
+#include <rdma/fi_errno.h>
+
 using namespace std;
+
+static struct fi_info *hints;
 
 int main(int argc, char const *argv[])
 {
