47584fe90ab5456900376b26fe7e3c01d73c6727
Dwarakanandan <dwarakanandan.bm@gmail.com>
2021-10-15 02:02:51 +0200

    Add common headers

--- src/common.h
+++ src/common.h
@@ -1,6 +1,28 @@
 #include <iostream>
+
+#include <time.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <assert.h>
+#include <getopt.h>
+#include <inttypes.h>
+#include <netdb.h>
+#include <poll.h>
+#include <limits.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+
 #include <rdma/fabric.h>
+#include <rdma/fi_cm.h>
+#include <rdma/fi_domain.h>
+#include <rdma/fi_endpoint.h>
+#include <rdma/fi_eq.h>
 #include <rdma/fi_errno.h>
+#include <rdma/fi_tagged.h>
 
 #include "cmd_line.h"
 
