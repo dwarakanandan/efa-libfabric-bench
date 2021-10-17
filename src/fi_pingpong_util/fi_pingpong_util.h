1e9c828512215ca9e09f4cd76a76992fcf9090d5
Dwarakanandan <dwarakanandan.bm@gmail.com>
2021-10-17 03:01:26 +0200

    Moved perf print to fi util

--- src/fi_pingpong_util/fi_pingpong_util.h
+++ src/fi_pingpong_util/fi_pingpong_util.h
@@ -120,3 +120,5 @@ char *size_str(char *str, uint64_t size);
 char *cnt_str(char *str, size_t size, uint64_t cnt);
 
 long parse_ulong(char *str, long max);
+
+void show_perf(char *name, int tsize, int sent, int acked, uint64_t start, uint64_t end, int xfers_per_iter);
